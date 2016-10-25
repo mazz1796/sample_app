a680ed002db8272639992c0b56d395a5b6712263
Koji Hiramatsu <mazz1796@gmail.com>
2016-10-25 11:08:31 +0800

    edit user

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -8,3 +8,19 @@ def full_title(page_title)
 end
 
 #to park this method called full title  under spec/support/utilities because it serves as a supporting method so that the spec test examples know what is method full_title..
+
+
+
+def sign_in(user, options={})  #The method is to visit the signin page and submit valid information
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
